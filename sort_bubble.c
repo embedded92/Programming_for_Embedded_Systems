2f2fb106e21c727ec3ca2994982fe2de21a2d16a
Ramneet Kaur <ramneetkaur.ece92@gmail.com>
2019-10-02 21:35:36 -0500

    Update sort_bubble.c

--- sort_bubble.c
+++ sort_bubble.c
@@ -1,52 +1,75 @@
-
-
 /*
- * bubble_sort.cpp
+ * bubble_inCpp.cpp
  *
- *  Created on: Sep 26, 2019
+ *  Created on: Oct 2, 2019
  *      Author: ramneet
  */
-// A function to implement bubble sort
+
+#include<iostream>
 #include <stdio.h>
-
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-void bubbleSort(int arr[], int n)
-{
-   int i, j;
-   for (i = 0; i < n-1; i++)
-
-       // Last i elements are already in place
-       for (j = 0; j < n-i-1; j++)
-           if (arr[j] > arr[j+1])
-              swap(&arr[j], &arr[j+1]);
-}
-
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("n");
-}
-
-// Driver program to test above functions
+using namespace std;
 int main()
 {
-    int arr[] = {64, 45,21,3,5,78,100, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    bubbleSort(arr, n);
-    printf("Sorted array: \n");
-    printArray(arr, n);
+    int n;
+    int order;
+    cout<<"Enter number of element you want to store: ";
+    cin>>n;
+    int arr[n],i,j=0;
+    cout<<"Enter array values:\n";
+
+    //taking the array value to sort from user
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter the order in which you want sorting: "
+    		"Ascending = 0"
+    		"Descending =1\n";
+    cin>>order;
+
+    if(order==0)
+    {
+    	   for(i=0;i<n-1;i++)
+    	    {
+    	        for(j=0;j<n-i-1;j++)
+    	        {
+    	            //checking if previous value is
+    	            //grater than next one or not
+    	            if(arr[j]<arr[j+1])
+    	            {
+    	                //temp will temporarly store
+    	                //the value of arr[j]
+    	                //then we will swap the values
+    	                int temp=arr[j];
+    	                arr[j]=arr[j+1];
+    	                arr[j+1]=temp;
+    	            }
+    	        }
+    	    }
+    }
+    if(order==1)
+    {
+    	   for(i=0;i<n-1;i++)
+    	    {
+    	        for(j=0;j<n-i-1;j++)
+    	        {
+    	            //checking if previous value is
+    	            //grater than next one or not
+    	            if(arr[j]>arr[j+1])
+    	            {
+    	                //temp will temporarly store
+    	                //the value of arr[j]
+    	                //then we will swap the values
+    	                int temp=arr[j];
+    	                arr[j]=arr[j+1];
+    	                arr[j+1]=temp;
+    	            }
+    	        }
+    	    }
+    }
+    cout<<"After Bubble sort the array is:\n";
+    for(i=0;i<n;i++)
+    cout<<arr[i]<<" ";
     return 0;
 }
-
-
-
 

acd32813c277828979c460c424c10a1120b46d48
Ramneet Kaur <ramneetkaur.ece92@gmail.com>
2020-08-25 20:21:11 -0500

    Updating UART communication- Arduino and BBB

---
+++ Arduino and BBB/task_2_bbbArduino/task_2_bbbArduino.ino
@@ -0,0 +1,30 @@
+
+ char x;
+ int led=false;
+ 
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+ pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  
+ // put your main code here, to run repeatedly:
+//Serial.println("HELLO");
+
+if (Serial.available())
+{ 
+   x= Serial.read();
+  
+  if(x=='a')
+  {
+    led=!led;
+    digitalWrite(LED_BUILTIN,led);   // turn the LED on (HIGH is the voltage level)
+    delay(100); 
+    Serial.print("LED IS = ");
+    Serial.println(led);
+     }
+}
+}
